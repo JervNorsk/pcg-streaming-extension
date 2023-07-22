15eaa492a875defbdab88abbdf162b863bd49dd1
JervNorsk <jerv.norsk@gmail.com>
2023-07-22 11:11:53 +0200

    Update project structure

---
+++ services/app/src/main/typescript/io/github/jervnorsk/extensions/pcg/streaming/services/app/features/timer/pce-se-timer.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  template: ''
+})
+export class PcgSeTimerComponent implements OnInit {
+
+  ngOnInit() {
+    window.location.replace(
+        "https://poketwitch.bframework.de/info/events/spawn_cooldown/"
+    )
+  }
+}
